46cfab5c4ae99e6461e61e0bc1ea6d197e7ed15e
aicouncil <56471284+aicouncil@users.noreply.github.com>
2025-06-05 12:17:01 +0530

    Add files via upload

---
+++ text_gen_app.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import google.generativeai as genai
+
+#configure api
+genai.configure(api_key="AIzaSyAC4EdU_8nr4wQgHN1Gt_u12Ffrg4u-bhU")
+
+#Create gemini model instance
+model = genai.GenerativeModel('models/gemini-2.0-flash')
+
+#Streamli UI
+st.set_page_config(page_title="Our Generative text app powered by Gemini")
+st.title("What do you want to know?")
+st.markdown("Tell us what information do you want, I will provide you the details.")
+
+#prompt input
+prompt_input = st.text_input("Provide your prompt here", value="")
+
+#Generate button
+if st.button("Generate"):
+    with st.spinner("Thinking...."):
+        prompt = prompt_input.strip()
+        try:
+            response = model.generate_content(prompt)
+            st.write(response.text)
+        except Exception as err:
+            st.error(f"An error occured - {err}")
